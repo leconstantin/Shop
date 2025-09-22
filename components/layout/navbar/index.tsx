633c326193c9e9d1b453c462cacb60013257d5f8
Leo Constantin <lcon69184@gmail.com>
2025-09-22 22:26:48 +0200

    added serach on navbar

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,8 +1,56 @@
+import Link from "next/link";
+import { Suspense } from "react";
+import LogoSquare from "@/components/logo-square";
 import { getMenu } from "@/lib/shopify";
+import type { Menu } from "@/lib/shopify/types";
+import MobileMenu from "./mobile-menu";
+import Search from "./search";
 
 export async function Navbar() {
   const menu = await getMenu("nextjs-frontend-menu");
-  console.log(menu);
+  //   console.log(menu);
 
-  return <div>Navbar</div>;
+  return (
+    <nav className="sticky top-0 z-30 flex items-center justify-between p-4 backdrop-blur-sm lg:px-6">
+      <div className="block flex-none md:hidden">
+        <Suspense fallback={null}>
+          <MobileMenu menu={menu} />
+        </Suspense>
+      </div>
+      <div className="flex w-full items-center">
+        <div className="flex w-full md:w-1/3">
+          <Link
+            className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
+            href={"/"}
+            prefetch={true}
+          >
+            <LogoSquare />
+            <div className="ml-2 flex-none font-medium text-sm uppercase md:hidden lg:block">
+              {process.env.SITE_NAME}
+            </div>
+          </Link>
+
+          {menu.length > 0 ? (
+            <ul className="hidden gap-6 text-sm md:flex md:items-center">
+              {menu.map((item: Menu) => (
+                <li key={item.title}>
+                  <Link
+                    className="text-gray-700 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
+                    href={item.path}
+                    prefetch={true}
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
+        <div className="hidden justify-center md:flex md:w-1/3">
+          <Search />
+        </div>
+        <div className="flex justify-end md:w-1/3">{/* <CartModal /> */}</div>
+      </div>
+    </nav>
+  );
 }
