2346232421fefa99781a097816e10b9ed17b6a13
Leo Constantin <lcon69184@gmail.com>
2025-09-23 21:09:23 +0200

    added api route

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,6 @@
+import type { NextRequest, NextResponse } from "next/server";
+import { revalidate } from "@/lib/shopify";
+
+export function POST(req: NextRequest): Promise<NextResponse> {
+  return revalidate(req);
+}
